7b95a4f1205650a5b257bd18db759d257b2d7638
Thomas Lundquist <thomasez@bisonlab.no>
2026-09-24 09:28:27 +0200

    More deprecationfixes.

--- src/DependencyInjection/AceEditorExtension.php
+++ src/DependencyInjection/AceEditorExtension.php
@@ -9,7 +9,7 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\UX\StimulusBundle\StimulusBundle;
 
 class AceEditorExtension extends Extension implements PrependExtensionInterface
